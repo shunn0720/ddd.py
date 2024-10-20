a0446e96c7b6974a4cca7ddf312f6d39a444b758
shunnn <ks.0720@gmail.com>
2024-10-20 18:44:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -71,7 +71,7 @@ class CommentModal(Modal):
         embed.set_author(name=self.user.display_name, icon_url=self.user.avatar.url)
         embed.add_field(
             name="リアクション結果",
-            value=f"{self.interaction.user.display_name} が '{self.label}' を押しました。",
+            value=f"{interaction.user.display_name} が '{self.label}' を押しました。",
             inline=False
         )
         embed.add_field(
@@ -91,6 +91,9 @@ class ReactionButton(Button):
         self.user = user
 
     async def callback(self, interaction: discord.Interaction):
+        # 応答を遅延させてエラーを防ぐ
+        await interaction.response.defer()
+
         # コメントを入力するためのモーダルを表示
         modal = CommentModal(label=self.label, user=self.user, interaction=interaction)
         await interaction.response.send_modal(modal)
