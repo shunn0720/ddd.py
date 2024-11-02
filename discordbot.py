f7e083bb9aa2a8354f130fa45feefdaa7ce7ebe1
shunnn <ks.0720@gmail.com>
2024-11-02 15:28:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,14 +23,14 @@ THREAD_PARENT_CHANNEL_ID = 1299231693336743996  # スレッドが作成される
 
 # ボタンの選択肢とスコア
 reaction_options = [
-    {"label": "入ってほしい！", "color": discord.Color.green(), "score": 2, "custom_id": "type1"},
-    {"label": "良い人！", "color": discord.Color.green(), "score": 1, "custom_id": "type2"},
-    {"label": "微妙", "color": discord.Color.red(), "score": -1, "custom_id": "type3"},
-    {"label": "入ってほしくない", "color": discord.Color.red(), "score": -2, "custom_id": "type4"}
+    {"label": "入ってほしい！", "color": discord.ButtonStyle.green, "score": 2, "custom_id": "type1"},
+    {"label": "良い人！", "color": discord.ButtonStyle.green, "score": 1, "custom_id": "type2"},
+    {"label": "微妙", "color": discord.ButtonStyle.red, "score": -1, "custom_id": "type3"},
+    {"label": "入ってほしくない", "color": discord.ButtonStyle.red, "score": -2, "custom_id": "type4"}
 ]
 
-# ボタンを押したユーザーのスレッドを追跡する辞書
-user_threads = {}
+# ユーザーのスレッドごとの投票を追跡する辞書
+user_vote_messages = {}
 
 # Bot設定
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -52,25 +52,27 @@ class CommentModal(Modal):
 
     async def on_submit(self, interaction: discord.Interaction):
         option = reaction_options[self.reaction_type]
-        embed = discord.Embed(color=option['color'])
+        embed = discord.Embed(color=option['color'].value)
         embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
         embed.add_field(name="リアクション結果", value=f"{interaction.user.display_name} が '{option['label']}' を押しました。", inline=False)
         embed.add_field(name="点数", value=f"{option['score']}点", inline=False)
         embed.add_field(name="コメント", value=self.comment.value if self.comment.value else "コメントなし", inline=False)
 
-        try:
-            await self.thread.send(embed=embed)
-            await interaction.response.send_message("投票を完了しました！", ephemeral=True)
-        except AttributeError:
-            logger.error("スレッドが見つかりません。")
-            await interaction.response.send_message("スレッドが見つかりませんでした。", ephemeral=True)
+        # 重複投票がある場合、古い投票を削除
+        if interaction.user.id in user_vote_messages and user_vote_messages[interaction.user.id] in self.thread.messages:
+            old_vote = user_vote_messages[interaction.user.id]
+            await old_vote.delete()
+
+        # 新しい投票メッセージをスレッドに送信
+        vote_message = await self.thread.send(embed=embed)
+        user_vote_messages[interaction.user.id] = vote_message
+
+        await interaction.response.send_message("投票を完了しました！", ephemeral=True)
 
 # ボタンを作成するクラス
 class ReactionButton(Button):
     def __init__(self, label, color, score, custom_id, reaction_type, thread):
-        super().__init__(label=label, style=discord.ButtonStyle.primary, custom_id=custom_id)
-        self.color = color
-        self.score = score
+        super().__init__(label=label, style=color, custom_id=custom_id)
         self.reaction_type = reaction_type
         self.thread = thread
 
@@ -80,7 +82,7 @@ class ReactionButton(Button):
 
 # Viewにボタンを追加
 def create_reaction_view(thread):
-    view = View(timeout=None)  # timeout=Noneでボタンが消えないように設定
+    view = View(timeout=None)
     for i, option in enumerate(reaction_options):
         view.add_item(ReactionButton(label=option["label"], color=option["color"], score=option["score"], custom_id=option["custom_id"], reaction_type=i, thread=thread))
     return view
@@ -98,9 +100,11 @@ async def on_message(message):
             logger.error("転記先チャンネルが見つかりません。")
             return
 
-        # メッセージの送信者のEmbedを作成して転記
+        # Embed作成：アイコンを大きく右に表示
         embed = discord.Embed(color=discord.Color.blue())
-        embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
+        embed.set_author(name=message.author.display_name)
+        embed.set_thumbnail(url=message.author.display_avatar.url)
+
         embed.add_field(
             name="🌱つぼみ審査投票フォーム",
             value="必ずこのサーバーでお話した上で投票をお願いします。\n複数回投票した場合は、最新のものを反映します。\nこの方の入場について、NG等意見のある方はお問い合わせください。",
@@ -115,7 +119,6 @@ async def on_message(message):
         thread_parent_channel = bot.get_channel(THREAD_PARENT_CHANNEL_ID)
         if thread_parent_channel:
             thread = await thread_parent_channel.create_thread(name=f"{message.author.display_name}のリアクション投票スレッド", auto_archive_duration=10080)
-            user_threads[message.author.id] = thread
             view = create_reaction_view(thread)
             await sent_message.edit(view=view)
             logger.info(f"スレッドが作成されました: {thread.id} for {message.author.display_name}")
